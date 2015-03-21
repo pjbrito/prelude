c77be86857e7df9c0d0f4c6387eaf5bae8fc9521
Miguel Vitorino <mvitorino@gmail.com>
2015-03-21 23:01:31 +0000

    golang settings

--- personal/init.el
+++ personal/init.el
@@ -1,3 +1,124 @@
-(prelude-require-packages '(solarized-theme window-numbering))
+(prelude-require-packages '(solarized-theme
+                            window-numbering
+                            go-eldoc
+                            go-autocomplete
+                            dockerfile-mode
+                            ;;desktop-save-mode
+                            ;;battery
+                            ))
+;; - rust-mode
+;; - go-mode
+;; - omnisharp
+;; - jedi  (for Python autocomplete)
 
 (window-numbering-mode 1)
+
+;; automatically save/restore opened files, windows config (number of windows, size, position), when quit ＆ restart emacs.
+(desktop-save-mode 1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;  Misc Settings
+;;
+
+;; http://stackoverflow.com/questions/69934/set-4-space-indent-in-emacs-in-text-mode
+;; 4 space identation
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;  GoLang
+;;
+
+(setenv "GOPATH" (concat (getenv "HOME") "/workspace/go-path/"))
+;(setenv "GOROOT" (concat (getenv "HOME") "/bin/google-cloud-sdk/platform/google_appengine/goroot/"))
+
+;; Go setup
+;; run this to get the dependencies:
+;; go get -u github.com/nsf/gocode
+;; go get -u code.google.com/p/rog-go/exp/cmd/godef
+;; go get -u github.com/dougm/goflymake
+;; go get github.com/smartystreets/goconvey
+
+;; OS X
+;;(if  (file-directory-p "/usr/local/opt/go/libexec/misc/emacs/")
+;;    (add-to-list 'load-path "/usr/local/opt/go/libexec/misc/emacs/"))
+;; Ubuntu
+;;(if  (file-directory-p "/usr/local/go/misc/emacs/")
+;;    (add-to-list 'load-path "/usr/local/go/misc/emacs/"))
+;; (add-to-list 'load-path (concat (live-pack-lib-dir) "go-mode.el/"))
+
+;; http://godoc.org/code.google.com/p/go.tools/cmd/goimports
+
+;;(require 'go-mode-load)
+(require 'go-mode-autoloads)
+(setq gofmt-command "goimports")
+
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "C-c i") 'go-goto-imports)))
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "M-.") 'godef-jump)))
+
+;; gocode for autocomplete
+(add-to-list 'load-path "~/workspace/go-path/src/github.com/nsf/gocode/emacs/")
+(require 'go-autocomplete)
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; go-flymake
+;(add-to-list 'load-path "~/workspace/go-path/src/github.com/dougm/goflymake")
+;(require 'go-flymake)
+
+;;(add-to-list 'load-path "~/workspace/go-path/src/github.com/dougm/goflymake")
+;;(require 'go-flycheck)
+
+;; TODO
+;; go-errcheck
+;; (add-to-list 'load-path "~/.live-packs/miguel-pack/lib/go-errcheck.el")
+;; (require 'go-errcheck)
+
+;; TODO https://github.com/dominikh/go-errcheck.el
+
+;; M-x package-install go-eldoc
+(add-hook 'go-mode-hook 'go-eldoc-setup)
+
+(set-face-attribute 'eldoc-highlight-function-argument nil
+                    :underline t :foreground "green"
+                    :weight 'bold)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;    EShell
+;;
+;; http://www.howardism.org/Technical/Emacs/eshell-fun.html
+(defun eshell-here ()
+  "Opens up a new shell in the directory associated with the
+current buffer's file. The eshell is renamed to match that
+directory to make multiple eshell windows easier."
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+                     (file-name-directory (buffer-file-name))
+                   default-directory))
+         (height (/ (window-total-height) 3))
+         (name   (car (last (split-string parent "/" t)))))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))
+
+    (insert (concat "ls"))
+    (eshell-send-input)))
+
+(global-set-key (kbd "C-!") 'eshell-here)
+
+
+(defun eshell/x ()
+  (insert "exit")
+  (eshell-send-input)
+  (delete-window))
--- personal/preload/init.el
+++ personal/preload/init.el
@@ -1,10 +1,39 @@
 
-
 ;; Prelude does not mess by default with the standard mapping of Command (to Super) and Option (to Meta).
 ;; If you want to swap them add this to your personal config:
-(setq mac-command-modifier 'meta)
-(setq mac-option-modifier 'super)
+;; Change meta key to cmd
+;; http://stackoverflow.com/questions/7743402/how-can-i-change-meta-key-from-alt-to-cmd-on-mac-in-emacs-24
+(setq mac-command-modifier 'meta
+      mac-option-modifier 'super)
 
 ;;(setq prelude-theme 'solarized-dark)
 
 (toggle-scroll-bar -1)
+
+(setq prelude-whitespace nil)
+
+;;; You email address
+(setq user-mail-address "mvitorino@gmail.com")
+
+;; show battery percentage in mode line
+(setq battery-mode-line-format "[%b%p%%]") ;; "#%b %p %t" ;; "[%b%p%%]"
+(setq battery-load-critical 7)
+(display-battery-mode t)
+
+;; display current time
+;;(setq display-time-day-and-date nil)
+;;(setq display-time-24hr-format nil)
+(setq display-time-format " %H:%M"
+      display-time-default-load-average nil)
+(display-time-mode 1)
+
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+
+This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+;;(set-exec-path-from-shell-PATH)
